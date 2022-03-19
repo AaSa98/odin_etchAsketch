d7cbbf52aed21c97e8ec54116a77ab3d91481f5b
AaSa98 <aa.sntmra@gmail.com>
2022-03-19 01:51:48 +0100

    inputfeld added

--- scripts.js
+++ scripts.js
@@ -1,11 +1,21 @@
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16, 1fr)";
+function populateBoard(size) {
+  if (size > 100) {
+    size = 100;
+  }
+  if (size < 1) {
+    size = 1;
+  }
+  let board = document.querySelector(".board");
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-//256 weil 16x16
-
-for (let i = 0; i < 256; i++) {
-  let square = document.createElement("div");
-  square.style.backgroundColor = "blue";
-  board.insertAdjacentElement("beforeend", square);
+  for (let i = 0; i < Math.pow(size, 2); i++) {
+    let square = document.createElement("div");
+    square.style.backgroundColor = "gray";
+    board.insertAdjacentElement("beforeend", square);
+  }
 }
+
+// submit_btn
+// number_input
+// wenn submit_btn click, dann populateBoard(text_input), default 16
