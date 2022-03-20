f1fc6e08ae395232b417dc2d19124c875af9fb44
AaSa98 <aa.sntmra@gmail.com>
2022-03-20 16:45:17 +0100

    farbänderung per hex code eingabe realisiert, auch bei änderung der Gridgröße funktioniert es

--- scripts.js
+++ scripts.js
@@ -12,9 +12,9 @@ function populateBoard(size) {
   for (let i = 0; i < Math.pow(size, 2); i++) {
     let square = document.createElement("div");
     square.style.backgroundColor = "gray";
-    square.addEventListener("mouseover", () => {
-      square.style.backgroundColor = "black";
-    });
+    // square.addEventListener("mouseover", () => {
+    //   square.style.backgroundColor = "black";
+    // });
     board.insertAdjacentElement("beforeend", square);
   }
 }
@@ -27,14 +27,26 @@ function clearBoard() {
 
 function changeSize(input) {
   const sliderValue = document.getElementById("sliderValue");
+  const colorPick = document.getElementById("colorPick");
   sliderValue.textContent = input;
   clearBoard();
   populateBoard(input);
+  //.textContent oder .innerHTML funktioniert nicht! Wert muss über .value aufgerufen werden!
+  changeColor(colorPick.value);
+}
+
+function changeColor(colorPick){
+  let board = document.querySelector(".board");
+  let pixels = board.querySelectorAll("div");
+  pixels.forEach((pixel) => (pixel.addEventListener("mouseover", () => {
+    pixel.style.backgroundColor = colorPick;
+  })));
 }
 
 function loadPage() {
   populateBoard(16);
   sliderValue.textContent = "16";
+  changeColor("black");
 }
 
 loadPage();
